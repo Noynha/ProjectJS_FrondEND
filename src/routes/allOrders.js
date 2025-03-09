2051353d74f8af84f6eeac993b035df588fc8b54
nannysuchawadi <147577225+nannysuchawadi@users.noreply.github.com>
2025-03-09 23:28:18 +0700

    create allorders

---
+++ src/routes/allOrders.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+
+const allOrdersRouter = require('express').Router()
+
+allOrdersRouter.get('/', async (req, res) => {
+  try {
+    const { data: dataProduct } = await axios.get('/product/')
+    const { data: dataProgram } = await axios.get('/program/')
+    const { data: dataCustumer } = await axios.get('/customer/')
+    const { data: dataOrders } = await axios.get('/orders/')
+    
+    res.render('booking', {
+      list_product: dataProduct.data,
+      list_program: dataProgram.data,
+      list_customer: dataCustumer.data,
+      list_orders: dataOrders.data
+    })
+  } catch (error) {
+    res.send('Error!!')
+  }
+})
+
+
+module.exports = allOrdersRouter
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const bookingRouter = require('./booking')
+const allOrdersRouter = require('./allOrders')
 
 // Routes (หน้าหลัก/หน้าแรก)
 router.get('/', (req, res) => res.render('index', {
@@ -8,5 +9,6 @@ router.get('/', (req, res) => res.render('index', {
 
 // Other Routes หน้าอื่น ๆ 
 router.use('/booking', bookingRouter)
-
+router.use('/allOrders',allOrdersRouter)
+ 
 module.exports = router
