79a637c7cdcee2fe51539ee2475e4ced9a2f1e76
csnpcjexpress <84871549+csnpch@users.noreply.github.com>
2025-03-09 23:55:14 +0700

    Remove unused

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,7 @@ const bookingRouter = require('./booking')
 const allOrdersRouter = require('./all_orders')
 
 // Routes (หน้าหลัก/หน้าแรก)
-router.get('/', (req, res) => res.render('index', {
-  name: 'Ham'
-}))
+router.get('/', (req, res) => res.render('index'))
 
 // Other Routes หน้าอื่น ๆ 
 router.use('/booking', bookingRouter)
