c7a74de9518323a9a32df56a49697ad5c6b98c40
nannysuchawadi <147577225+nannysuchawadi@users.noreply.github.com>
2025-03-09 23:53:43 +0700

    change all_orders

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const bookingRouter = require('./booking')
-const allOrdersRouter = require('./allOrders')
+const allOrdersRouter = require('./all_orders')
 
 // Routes (หน้าหลัก/หน้าแรก)
 router.get('/', (req, res) => res.render('index', {
@@ -9,6 +9,6 @@ router.get('/', (req, res) => res.render('index', {
 
 // Other Routes หน้าอื่น ๆ 
 router.use('/booking', bookingRouter)
-router.use('/allOrders',allOrdersRouter)
+router.use('/all_orders',allOrdersRouter)
  
 module.exports = router
